8b09cb9ce66843467d8192b10f4d0fb3dfd9f7f2
lsatoshi <leandro.satoshi@usp.br>
2021-06-21 14:42:48 -0300

    added lerp to moveable platform

--- Assets/MovablePlatform.cs
+++ Assets/MovablePlatform.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class MovablePlatform : MonoBehaviour, Activatable {
     public GameObject platform;
     public Transform[] positionsArray;
-    public Animator anim;
-    public int state = 0;
+    public int position = 0;
+    private bool isMoving = false;
+    private Vector3 curr;
+    private float i = 0;
 
     public void Start() {
-        
+        platform.GetComponent<Transform>().position = positionsArray[position].position;
     }
 
     public void OnActivate() {
-        ChangeState();
+        ChangePosition();
     }
 
-    void ChangeState() {
-        if(state < 1) {
-            state++;
-        }else state = 0;
-        platform.GetComponent<Transform>().position = positionsArray[state].position;
+    private void FixedUpdate() {
+        if(isMoving) {
+            platform.GetComponent<Transform>().position = Vector3.Lerp(curr, positionsArray[position].position, i);
+            if(i > 1) {
+                isMoving = false;
+                i = 0;
+            }
+            i += 0.05f;
+        }
+    }
+
+
+    void ChangePosition() {
+        if(position < positionsArray.Length - 1) {
+            position++;
+        }else position = 0;
+        curr = platform.GetComponent<Transform>().position;
+        isMoving = true;
     }
 }
